84f63f5eda1a7c392fa3b19b5d5a3af722ea2cec
Saif Azmi <saif.azmi0108@gmail.com>
2016-03-12 21:03:28 +0000

    Removed useless variables and fixed MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,40 +1,51 @@
 import shell_engine
-import urllib
 import add_task
 from trello import TrelloApi
+
 appKey = "bbe4f72dcc11de483fec87b87db05532"
-trelloToken = ""
-trello
-boardID = 0
+
 
 def view():
     print("view")
+
+
 def add():
     print("add")
-    add_task.newTask(trello)
+    add_task.newTask(init(), getBoard())
+
+
 def start():
     print("start")
+
+
 def submit():
     print("submit")
+
+
 def review():
     print("review")
 
+
 def init():
-    trello = TrelloApi(appKey)
+    tr = TrelloApi(appKey)
     print("Please visit the following website and copy the key:")
     print('https://trello.com/1/authorize?key=' + appKey +
           '&name=TVC_Commit&expiration=30days&response_type=token&scope=read,write')
     trelloToken = input("Please enter key: ")
+    tr.set_token(trelloToken)
+    return tr
+
+
+def getBoard():
     boardID = input("Please enter board id: ")
-    trello.set_token(trelloToken)
+    return boardID
 
-
-options ={
-    1 : view,
-    2 : add,
-    3 : start,
-    4 : submit,
-    5 : review,
+options = {
+    1: view,
+    2: add,
+    3: start,
+    4: submit,
+    5: review,
 }
 
 if __name__ == '__main__':
@@ -52,4 +63,3 @@ if __name__ == '__main__':
     choice = input("Please Enter Your Choice: ")
     shell_engine.runShellCommand("clear")
     options[choice]()
-
--- add_task.py
+++ add_task.py
@@ -1,7 +1,3 @@
-import userpass
-from trello import TrelloApi
-from trello import boards
-
 def newTask(trello, boardID):
     lists = trello.boards.get_list(boardID)
     print(lists)
