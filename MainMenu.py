4e231df836c769bd0c0a7878584ce9d7338e8e9b
Brad Rowe <bradleyrowe@live.com>
2016-03-12 20:46:11 +0000

    Added a gitignore

--- MainMenu.py
+++ MainMenu.py
@@ -1,15 +1,33 @@
 import shell_engine
+import urllib
+import add_task
+from trello import TrelloApi
+appKey = "bbe4f72dcc11de483fec87b87db05532"
+trelloToken = ""
+trello
+boardID = 0
 
 def view():
     print("view")
 def add():
     print("add")
+    add_task.newTask(trello)
 def start():
     print("start")
 def submit():
     print("submit")
 def review():
     print("review")
+
+def init():
+    trello = TrelloApi(appKey)
+    print("Please visit the following website and copy the key:")
+    print('https://trello.com/1/authorize?key=' + appKey +
+          '&name=TVC_Commit&expiration=30days&response_type=token&scope=read,write')
+    trelloToken = input("Please enter key: ")
+    boardID = input("Please enter board id: ")
+    trello.set_token(trelloToken)
+
 
 options ={
     1 : view,
@@ -20,6 +38,7 @@ options ={
 }
 
 if __name__ == '__main__':
+
     print("""
     Welcome to the TVC main menu
     ============================
