3259fe0aa2dcfbf104b9f28564daa7d36394b2fe
Connagh Jacobi <cxj429@bham.ac.uk>
2016-03-13 01:27:57 +0000

    edited user input

--- add_task.py
+++ add_task.py
@@ -3,5 +3,6 @@ import checkLists
 def newTask(trello, boardID):
 	listId = checkLists.getTODOList(trello,boardID)
 	name = str(raw_input("Name of card: "))
+	name = name +" [" +str(raw_input("Please add a branch name: "))+"]"
 	description = str(raw_input("desc of card:\n"))
 	trello.cards.new(name,listId,description)
