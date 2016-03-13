66328b796402e86922b1a603e7a127aad5c1f3fd
Connagh Jacobi <cxj429@bham.ac.uk>
2016-03-13 11:14:27 +0000

    added final commit

--- submit_task.py
+++ submit_task.py
@@ -1,4 +1,5 @@
 import checkLists
+import shell_engine
 
 def submitTask(trello,board):
 	listid = checkLists.getInProgList(trello,board)
@@ -12,5 +13,10 @@ def submitTask(trello,board):
 	cardId= cards[indexSelected]['id']
 	submitId = checkLists.getSubmittedList(trello,board)	
 	trello.cards.update_idList(cardId, submitId)
+	comment= str(raw_input("Comment for final Commit: "))
+	cmd = "git commit -a -m " + "'"+comment+"'"
+	print (cmd)
+	shell_engine.runShellCommand(cmd)
+	trello.cards.new_action_comment(cardId,cmd)
 
 
