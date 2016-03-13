0a8071c4fd7f9ff2fb26df4d4aa4bf69868881ae
Brad Rowe <bradleyrowe@live.com>
2016-03-13 03:09:32 +0000

    Printing in tables

--- review_task.py
+++ review_task.py
@@ -1,4 +1,4 @@
-
+import checkLists
 
 def reviewTask(trello,board):
 	listid = checkLists.getSubmittedList(trello,board)
@@ -8,10 +8,10 @@ def reviewTask(trello,board):
 		print("{} {}".format(count,item['name']))
 		count = count +1
 
-	indexSelected = int(raw_input("Select card to accept/reject: "))
+	indexSelected = int(raw_input("Select card to review: "))
 	cardId= cards[indexSelected]['id']
 	while (True):
-		ans = str(raw_input("Enter Y to accept or N to reject: "))
+		ans = str(raw_input("Accept submitted solution [y/n]: "))
 		if ans.lower() == "y":		
 			doneId = checkLists.getDoneList(trello,board)	
 			trello.cards.update_idList(cardId, doneId)
@@ -19,7 +19,8 @@ def reviewTask(trello,board):
 		elif ans.lower()== "n":
 			progId = checkLists.getInProgList(trello,board)	
 			trello.cards.update_idList(cardId, progId)
-			reason = str(raw_input("Reason for rejection:"))
-			trello.card.new_action_comment(cardId,reason)
+			reason = str(raw_input("Reason for rejection:\n"))
+			reason = "Submit request rejected, due to: " + reason
+			trello.cards.new_action_comment(cardId,reason)
 			break
 	
