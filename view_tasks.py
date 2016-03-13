3f09b6a33f6fb5cae1c7a7fc16b631979c1c82f0
Connagh Jacobi <cxj429@bham.ac.uk>
2016-03-13 03:05:42 +0000

    damn

---
+++ view_tasks.py
@@ -0,0 +1,13 @@
+
+
+def viewTasks(trello, board):
+	lists = trello.boards.get_list(board)
+	count = 0
+	for item in lists:
+		print("{} {}".format(count, items['name']))
+		count = count +1
+
+	view = int(raw_input("Which list would you like to view? "))
+	viewlist = trello.lists.get_card(item[view]['id'])
+	for item in viewlist:
+		print("{} {}".format(item['name'],item["desc"]))
